b7361ccf9dbd9b0c59add0860c84810ed260327a
Sean Templeton <sean@templeton.io>
2022-09-29 12:13:29 -0500

    Reduce example code

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
 // Initialize New Relic
 import express from 'express';
-import cors from 'cors';
-
 import vehicleRoutes from './routes/vehicle.js';
 
 // Initialize app
 const app = express();
 const port = process.env.PORT;
-
-// Use JSON for API requests
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true }));
-
-// Prevent CORS restrictions during development
-app.use(
-    cors({
-        origin: '*',
-        methods: ['GET', 'POST', 'PUT'],
-    }),
-);
 
 // Set up endpoints
 app.get('/', (req, res) => res.sendStatus(200));
